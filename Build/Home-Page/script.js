f32b9fed9ba55ba53974d4ad9300480e4118f356
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2023-09-09 19:26:38 +0530

    moved some js from home to script.js

--- Build/Home-Page/script.js
+++ Build/Home-Page/script.js
@@ -1,1 +1,8 @@
-//bruh
+document.getElementById('toggleButtonClose').addEventListener('click', function() {
+    document.getElementById('menuItems').classList.toggle('hidden');
+});
+
+document.getElementById('openButton').addEventListener('click', function(){
+document.getElementById('menuItems').classList.toggle('hidden');
+}
+);
