6845be1e6f0affc5e66d342443b0d3f556fd2a24
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2023-09-13 01:35:35 +0530

    completed backend for contactpage

---
+++ Build/php/contactProcess.php
@@ -0,0 +1,29 @@
+<?php
+include("connection.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $message = $_POST["message"];
+
+    // Sanitize user input (for security)
+    $firstName = mysqli_real_escape_string($con, $firstName);
+    $lastName = mysqli_real_escape_string($con, $lastName);
+    $email = mysqli_real_escape_string($con, $email);
+    $phoneNumber = mysqli_real_escape_string($con, $phoneNumber);
+    $message = mysqli_real_escape_string($con, $message);
+
+    // Insert data into the database
+    $query = "INSERT INTO contact (firstName, lastName, email, phoneNumber, message) VALUES ('$firstName', '$lastName','$email', '$phoneNumber', '$message')";
+
+    if (mysqli_query($con, $query)) {
+        echo "Contact details added successfully!";
+    } else {
+        echo "Error: " . mysqli_error($con);
+    }
+
+    mysqli_close($con);
+}
+?>
