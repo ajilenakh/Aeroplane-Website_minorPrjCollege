22e4de3c0cf8460c0e275a3343621b6f5cfab222
Arindam <107887918+Arindam905@users.noreply.github.com>
2023-09-13 00:21:27 +0530

    fixed loginProcess

--- Build/php/loginProcess.php
+++ Build/php/loginProcess.php
@@ -1,120 +1,46 @@
 <?php
+session_start();
+
 include("connection.php");
 include("functions.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+$email = $_POST["email"];
+$password = $_POST["password"];
 
-    // Check if both email and password fields are filled
-    if (!empty($email) && !empty($password)) {
-        // Fetch user data based on the provided email
-        $query = "SELECT id, User_Id, username, email, passwordhash, salt FROM users WHERE email = ?";
-        $stmt = mysqli_prepare($con, $query);
+// Retrieve the stored salt and passwordhash for the given email from your database
+$query = "SELECT salt, passwordhash FROM users WHERE email = ?";
+$stmt = mysqli_prepare($con, $query);
 
-        if ($stmt) {
-            // Bind parameters with references
-            mysqli_stmt_bind_param($stmt, "s", $email);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_store_result($stmt);
+if ($stmt) {
+    // Bind parameters with references
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
 
-            // Check if a user with the given email exists
-            if (mysqli_stmt_num_rows($stmt) == 1) {
-                // Bind the result variables
-                mysqli_stmt_bind_result($stmt, $id, $UserId, $username, $FetchedEmail, $FetchedPasswordHash, $Salt);
-                mysqli_stmt_fetch($stmt);
+    // Check if a user with the given email exists
+    if (mysqli_stmt_num_rows($stmt) == 1) {
+        // Bind the result variables
+        mysqli_stmt_bind_result($stmt, $salt, $HashedPassword);
 
-                // Hash the provided password with the fetched salt
-                $hashed_password = password_hash($password . $Salt, PASSWORD_BCRYPT);
+        // Fetch the stored hashed password and salt from the database
+        mysqli_stmt_fetch($stmt);
 
-                // Compare the hashed password with the fetched password hash
-                if (password_verify($hashed_password, $FetchedPasswordHash)) {
-                    // Passwords match, user is authenticated
-                    session_start();
-                    $_SESSION["user_id"] = $UserId;
-                    $_SESSION["email"] = $FetchedEmail;
-                    $_SESSION["username"] = $username;
-                    header("Location: homePage.php");
-                    die();
-                } else {
-                    // Passwords don't match
-                    echo "Incorrect password. Please try again.";
-                }
-            } else {
-                // No user with the provided email exists
-                echo "No user found with that email.";
-            }
-
-            mysqli_stmt_close($stmt);
+        // Verify the password
+        if (password_verify($password . $salt, $HashedPassword)) {
+            // Passwords match, grant access
+            $_SESSION["email"] = $email; // Store user's email in the session
+            header("Location: homePage.php");
         } else {
-            echo "Error: " . mysqli_error($con);
+            // Invalid credentials
+            echo "Login failed. Please check your credentials.";
         }
     } else {
-        echo "Please fill in both the email and password fields.";
+        // No user with the provided email exists
+        echo "No user found with that email.";
     }
+
+    mysqli_stmt_close($stmt);
+} else {
+    echo "Error: " . mysqli_error($con);
 }
-
-
-
-
-
-
-
-
-/*session_start();
-
-include("connection.php");
-include("functions.php");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    // Check if both email and password fields are filled
-    if (!empty($email) && !empty($password)) {
-        // Fetch user data based on the provided email
-        $query = "SELECT UserId, email, passwordhash, salt FROM users WHERE email = ?";
-        $stmt = mysqli_prepare($con, $query);
-
-        if ($stmt) {
-            // Bind parameters with references
-            mysqli_stmt_bind_param($stmt, "s", $email);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_store_result($stmt);
-
-            // Check if a user with the given email exists
-            if (mysqli_stmt_num_rows($stmt) == 1) {
-                // Bind the result variables
-                mysqli_stmt_bind_result($stmt, $UserId, $FetchedEmail, $FetchedPasswordHash, $Salt);
-                mysqli_stmt_fetch($stmt);
-
-                // Hash the provided password with the fetched salt
-                $hashed_password = password_hash($password . $Salt, PASSWORD_BCRYPT);
-
-                // Compare the hashed password with the fetched password hash
-                if (password_verify($hashed_password, $FetchedPasswordHash)) {
-                    // Passwords match, user is authenticated
-                    $_SESSION["user_id"] = $UserId;
-                    $_SESSION["email"] = $FetchedEmail;
-                    header("Location: homePage.php");
-                    die();
-                } else {
-                    // Passwords don't match
-                    echo "Incorrect password. Please try again.";
-                }
-            } else {
-                // No user with the provided email exists
-                echo "No user found with that email.";
-            }
-
-            mysqli_stmt_close($stmt);
-        } else {
-            echo "Error: " . mysqli_error($con);
-        }
-    } else {
-        echo "Please fill in both the email and password fields.";
-    }
-}*/
-
-
 ?>
