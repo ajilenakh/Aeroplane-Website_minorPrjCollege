10eeecd943bc08dde6c214bd204d3e64ed2ba272
Arindam <107887918+Arindam905@users.noreply.github.com>
2023-09-12 21:27:26 +0530

    fixed script.js

--- Build/JS/script.js
+++ Build/JS/script.js
@@ -1,77 +1,84 @@
-
-//Opening button for hamburger menu
-document.getElementById('toggleButtonClose').addEventListener('click', function() {
-    document.getElementById('menuItems').classList.toggle('hidden');
+// Opening button for hamburger menu
+document.getElementById('toggleButtonClose').addEventListener('click', function () {
+  document.getElementById('menuItems').classList.toggle('hidden');
 });
 
-//Closing button for hamburger menu
-document.getElementById('openButton').addEventListener('click', function(){
-document.getElementById('menuItems').classList.toggle('hidden');
-}
-);
-
+// Closing button for hamburger menu
+document.getElementById('openButton').addEventListener('click', function () {
+  document.getElementById('menuItems').classList.toggle('hidden');
+});
 
 function changeTab(event, index) {
-            var tabButtons = document.getElementById('tab-buttons').children;
-            var tabPanels = document.getElementById('tab-panels').children;
+  var tabButtons = document.getElementById('tab-buttons').children;
+  var tabPanels = document.getElementById('tab-panels').children;
 
-            // Remove the active utility classes from all tabs (bg-white, text-blue-600)
-            // And hide all tab content (with the "hidden" utility)
-            for (var i = 0; i < tabButtons.length; i++) {
-                tabButtons[i].classList.remove('text-blue-600');
-                tabButtons[i].classList.remove('bg-white');
-                tabButtons[i].classList.add('text-white');
-                tabPanels[i].classList.add('hidden');
-            }
+  // Remove the active utility classes from all tabs (bg-white, text-blue-600)
+  // And hide all tab content (with the "hidden" utility)
+  for (var i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove('text-blue-600');
+      tabButtons[i].classList.remove('bg-white');
+      tabButtons[i].classList.add('text-white');
+      tabPanels[i].classList.add('hidden');
+  }
 
-            // Add the active utility classes to the currently active tab (bg-white, text-blue-600)
-            // And show the current tab content (remove the "hidden" utility)
-            tabButtons[index].classList.remove('text-white');
-            tabButtons[index].classList.add('text-blue-600');
-            tabButtons[index].classList.add('bg-white');
-            tabPanels[index].classList.remove('hidden');
-        }
-        
-    
-// switch between login and signup
-document.addEventListener('DOMContentLoaded', function() {
-    var registerButton = document.getElementById('registerButton');
-    registerButton.addEventListener('click', function() {
-        window.location.href = "../php/registerPage.php";});
-});
+  // Add the active utility classes to the currently active tab (bg-white, text-blue-600)
+  // And show the current tab content (remove the "hidden" utility)
+  tabButtons[index].classList.remove('text-white');
+  tabButtons[index].classList.add('text-blue-600');
+  tabButtons[index].classList.add('bg-white');
+  tabPanels[index].classList.remove('hidden');
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    var loginButton = document.getElementById('loginButton');
-    loginButton.addEventListener('click', function() {
-        window.location.href = "../php/loginPage.php";});
-});
+// Switch between login and signup
+document.addEventListener('DOMContentLoaded', function () {
+  var registerButton = document.getElementById('registerButton');
+  if (registerButton) {
+      registerButton.addEventListener('click', function () {
+          window.location.href = "../php/registerPage.php";
+      });
+  }
 
-
-// passwordToggle
-document.addEventListener('DOMContentLoaded', function() {
-    const passwordInput = document.getElementById('password');
-    const confirmPasswordInput = document.getElementById('confirmPassword');
-    const togglePassword = document.getElementById('togglePassword');
-  
-    togglePassword.addEventListener('click', function() {
-      if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        confirmPasswordInput.type = 'text';
-      } else {
-        passwordInput.type = 'password';
-        confirmPasswordInput.type = 'password';
-      }
-    });
-  });
-const passwordInput = document.getElementById('passwordInput');
-const togglePasswordVisibility = document.getElementById('togglePasswordVisibility');
-togglePasswordVisibility.style.cursor = 'pointer';
-  
-togglePasswordVisibility.addEventListener('click', function () {
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-  } else {
-    passwordInput.type = 'password';
+  var loginButton = document.getElementById('loginButton');
+  if (loginButton) {
+      loginButton.addEventListener('click', function () {
+          window.location.href = "../php/loginPage.php";
+      });
   }
 });
 
+// Password Toggle
+document.addEventListener('DOMContentLoaded', function () {
+  const passwordInput = document.getElementById('password');
+  const confirmPasswordInput = document.getElementById('confirmPassword');
+  const togglePassword = document.getElementById('togglePassword');
+
+  if (togglePassword) {
+      togglePassword.addEventListener('click', function () {
+          if (passwordInput.type === 'password') {
+              passwordInput.type = 'text';
+              confirmPasswordInput.type = 'text';
+          } else {
+              passwordInput.type = 'password';
+              confirmPasswordInput.type = 'password';
+          }
+      });
+  }
+});
+
+// Password Visibility Toggle
+document.addEventListener('DOMContentLoaded', function () {
+  const passwordInput = document.getElementById('passwordInput');
+  const togglePasswordVisibility = document.getElementById('togglePasswordVisibility');
+
+  if (togglePasswordVisibility) {
+      togglePasswordVisibility.style.cursor = 'pointer';
+
+      togglePasswordVisibility.addEventListener('click', function () {
+          if (passwordInput.type === 'password') {
+              passwordInput.type = 'text';
+          } else {
+              passwordInput.type = 'password';
+          }
+      });
+  }
+});
--- Build/php/registerPage.php
+++ Build/php/registerPage.php
@@ -7,24 +7,28 @@ session_start();
     //$user_data = check_login($con);
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-  if (!empty($UserName) && !empty($password) && !is_numeric($username)) {
-    //save to database
-    $UserId = random_num(20);
-    $query = "insert into users (UserId, username, email, password) values ('$UserId', '$Username', '$Email', '$Password')";
-    
-    mysqli_query($con ,$query);
-
-    header("Location: homePage.php");
-    die;
-  }else {
-    echo "Please enter some valid information!";
-  }  
-}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $username = $_POST["username"];
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+      
+      if (!empty($username) && !empty($password) && !is_numeric($username)) {
+          // Save to database using prepared statement
+          $UserId = random_num(20);
+          $query = "INSERT INTO users (UserId, username, email, password) VALUES (?, ?, ?, ?)";
+          $stmt = $con->prepare($query);
+          $stmt->bind_param("ssss", $UserId, $Username, $Email, $Password);
+          if ($stmt->execute()) {
+              header("Location: homePage.php");
+              die();
+          } else {
+              echo "Error: " . $stmt->error;
+          }
+          $stmt->close();
+      } else {
+          echo "Please enter some valid information!";
+      }
+  }
 ?>
 
 
