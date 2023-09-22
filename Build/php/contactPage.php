abe72b8b496da49382bd30be5d1ceba71029cbb8
Arindam <107887918+ajilenakh@users.noreply.github.com>
2023-09-22 20:37:04 +0530

    added placeholder for phone number

--- Build/php/contactPage.php
+++ Build/php/contactPage.php
@@ -130,7 +130,7 @@ if(isset($_SESSION['username'])){
             <div class="sm:col-span-2">
                 <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
                 <div class="mt-2.5">
-                  <input type="phone-number" required name="phoneNumber" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <input type="phone-number" placeholder="Optional" required name="phoneNumber" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
               </div>
             <div class="sm:col-span-2">
