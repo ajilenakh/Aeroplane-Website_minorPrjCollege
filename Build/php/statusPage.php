8fcd52e8360e339a1f324ba49c395c2d79c92a8c
Ajilenakh <107887918+ajilenakh@users.noreply.github.com>
2024-12-03 20:52:59 +0530

    Flight id to Flight ID

--- Build/php/statusPage.php
+++ Build/php/statusPage.php
@@ -103,12 +103,12 @@ $user_data = check_login($con);
         <ul class="relative flex list-none flex-wrap rounded-xl bg-blue-gray-50/60 p-1 lg:bg-gray-200" data-tabs="tabs" role="list">
           <li class="z-30 flex-auto text-center">
             <a class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out" data-tab-target="" active="" role="tab" aria-selected="true" aria-controls="app">
-              <span class="ml-1">Search by flight id</span>
+              <span class="ml-1">Search by FlightId</span>
             </a>
           </li>
           <li class="z-30 flex-auto text-center">
             <a class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out" data-tab-target="" role="tab" aria-selected="false" aria-controls="message">
-              <span class="ml-1">Search by route</span>
+              <span class="ml-1">Search by Route</span>
             </a>
           </li>
         </ul>
